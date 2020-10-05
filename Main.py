9c5f8c94d7a1ad4ddcc95f61203c82383bdd3967
SaaranshJain <53344035+SaaranshJain@users.noreply.github.com>
2020-10-05 11:17:41 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,123 @@
+from flask import Flask , render_template , url_for , flash , redirect
+from Forms import RegistrationForm , LoginForm , CreateSingle
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "4398cf5c355c410ccaa5b58856b98372"
+
+posts = [
+    {
+        "subject" : "English",
+        "title" : "Midterm Exam",
+        "portions" : '''Portions :
+
+        01. The Portrait of a Lady
+        02. A Photograph, We're not Afraid to Die....
+        03. Discovering Tut: The Saga Continues
+        04. The Laburnum Top
+        05. Landscape of the soul
+        06. The Voice of the Rain
+        07. The Summer of the Beautiful White Horse
+        08. The Address
+        09. Ranga's Mariage
+        10. Albert Einstein at School
+        11. Integrated grammar
+        12. Formal letters
+        13. Article Writing
+        14. Note making''',
+        "date_of_exam" : "October 7, 2020"
+    },
+    {
+        "subject" : "Computer",
+        "title" : "Midterm Exam",
+        "portions" : '''Portions : 
+
+        01. Getting started with python.
+        02. Python fundamentals.
+        03. Data Handling.
+        04. Conditional and iterative statements.
+        05. String Manipulation.
+        06. List Manipulation.
+        07. Tuples.
+        08. Computer system overview
+        09. Data Representation.''',
+        "date_of_exam" : "October 9, 2020"
+    },
+    {
+        "subject" : "Math",
+        "title" : "Midterm Exam",
+        "portions" : '''Portions :
+
+        01. Sets
+        02. Relations & Functions
+        03. Trigonometric Functions  
+        04. Complex Numbers & Quadratic Equations
+        05. Linear Inequalities
+        06. Sequences & Series
+        07. Permutations & Combinations''',
+        "date_of_exam" : "October 12, 2020"
+    },
+    {
+        "subject" : "Physics",
+        "title" : "Midterm Exam",
+        "portions" : '''Portions :
+
+        02. Units and Measurements 
+        03. Motion in a straight line
+        04. Motion in a plane
+        05. Laws of motion
+        06. Work,Energy and Power
+        07. Motion of system of particles''',
+        "date_of_exam" : "October 17, 2020"
+    },
+    {
+        "subject" : "Chemistry",
+        "title" : "Midterm Exam",
+        "portions" : '''Portions :
+
+        01. Some basic concepts in Chemistry
+        02. Structure of atom
+        03. Classification of elements and periodicity in properties
+        04. Chemical bonding
+        05. Thermodynamics
+        06. Chemical Equilibrium
+        07. Hydrogen''',
+        "date_of_exam" : "October 20, 2020"
+    }
+]
+
+@app.route("/")
+def home() :
+    return render_template("Home.html" , posts=posts , Title="Exam Overview")
+
+@app.route("/about")
+def about() :
+    return render_template("About.html" , Title="About Us")
+
+@app.route("/register" , methods=['GET' , 'POST'])
+def register() :
+    form_reg = RegistrationForm()
+    if form_reg.validate_on_submit() :
+        flash(f"Account created for {form_reg.name.data}!" , category="success")
+        return redirect(url_for("login"))
+    return render_template("Register.html" , Title="Register" , form = form_reg)
+
+@app.route("/login")
+def login() :
+    form_log = LoginForm()
+    return render_template("Login.html" , Title="Login" , form = form_log)
+
+@app.route("/create")
+def createsingle() :
+    form_createsingle = CreateSingle()
+    return render_template("CreateSingle.html" , Title="Create" , form = form_createsingle)
+
+@app.route("/calendar")
+def calendar() :
+    return "Calendar"
+
+@app.route("/createseries")
+def createseries() :
+    return "Series"
+
+if __name__ == "__main__" :
+    app.run(debug=True)
